2c8a415263a28c61acc4b9c65bda2a7b6a315512
Naveen AutomationLabs <naveenanimation20@gmail.com>
2022-08-29 07:57:23 +0530

    updated Selenoid Code

--- src/test/java/com/qa/opencart/base/BaseTest.java
+++ src/test/java/com/qa/opencart/base/BaseTest.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.asserts.SoftAssert;
 
 import com.qa.opencart.factory.DriverFactory;
@@ -17,34 +18,37 @@ import com.qa.opencart.pages.SearchResultsPage;
 public class BaseTest {
 	public WebDriver driver;
 	public Properties prop;
-	
+
 	public DriverFactory df;
-	
+
 	public LoginPage loginPage;
 	public AccountsPage accPage;
 	public SearchResultsPage searchResPage;
 	public ProductInfoPage productInfoPage;
 	public RegisterPage regPage;
-	
+
 	public SoftAssert softAssert;
-	
+
+	@Parameters({ "browser" , "browserversion"})
 	@BeforeTest
-	public void setup() {
+	public void setup(String browserName, String browserVersion) {
 		df = new DriverFactory();
-		
+
 		prop = df.initProp();
+
+		if (browserName != null) {
+			prop.setProperty("browser", browserName);
+			prop.setProperty("browserversion", browserVersion);
+		}
+
 		driver = df.initDriver(prop);
 		loginPage = new LoginPage(driver);
 		softAssert = new SoftAssert();
 	}
-	
+
 	@AfterTest
 	public void tearDown() {
 		driver.quit();
 	}
-	
-	
-	
-	
 
 }
